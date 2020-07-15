97ffdb66a4c5a17649065e55ab502b104d73931f
DragosPredi <predi.dragos@gmail.com>
2020-07-15 13:27:53 +0300

    Finished Driver class, the program now fully functional (should be at least)

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,8 +1,71 @@
 package main;
 
+import main.bills.Bill;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Driver {
+    /**
+     * Continuously asks the user for input until it receives an Int
+     * @return -> int
+     */
+    public static int readPositiveInt() {
+        Scanner scanner = new Scanner(System.in);
+        int cashAmount = -1;
+        try {
+            cashAmount = scanner.nextInt();
+            if(cashAmount <= 0){
+                throw new InputMismatchException();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("It would seem the input was faulty, please try again!");
+        }
+        return cashAmount;
+    }
+
+    /**
+     * Helper function to print in human readable format the bills requested
+     * @param bills -> the bills used
+     */
+    public static void printBills(int[] bills) {
+        int cont = 0;
+        if (bills[cont] == Integer.MAX_VALUE) {
+            System.out.println("The ATM is out of cash, please try again later!");
+        } else {
+            for (Bill.Type billType : Bill.Type.values()) {
+                System.out.println("Number of " + billType + " is " + bills[cont++]);
+            }
+        }
+    }
+
+    /**
+     * Helper function to complete the transaction, it takes an ATM obj
+     * and prints the bills used
+     * @param ATM -> ATM object
+     */
+    public static void transaction(AutomaticTellerMachine ATM){
+
+        int cashAmount;
+        do {
+            cashAmount = readPositiveInt();
+        } while (cashAmount <= 0);
+
+        printBills(ATM.withdrawalRequest(cashAmount));
+    }
+
+    public static String readString(){
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Setting up project!");
+        AutomaticTellerMachine ATM = new AutomaticTellerMachine();
+        do {
+            System.out.println("What sum would you like to withdraw? Please enter a positive number");
+            transaction(ATM);
+            System.out.println("Thank you for choosing our services, would you like to try again? (yes/no)");
+        } while (readString().equals("yes"));
+
     }
 }
